6a1dcbbb0d300a68a03f5f280cf316a8660e70b3
trishit <trishit.org@gmail.com>
2026-01-09 16:46:25 +0530

    Time: 6 ms (50.14%) | Memory: 55.6 MB (48.73%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,7 @@
-import java.util.HashMap;
 
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> freq = new HashMap<Integer,Integer>();
-        double threshold = nums.length/2;
-        int max = 0;
-        for(int i:nums)
-        {
-            freq.put(i,freq.getOrDefault(i,0)+1);
-            if(freq.get(i) > threshold)
-            max = i;
-        }
-        return max;
+    public int majorityElement(int[] nums) {  
+    Arrays.sort(nums);
+    return nums[nums.length/2];
     }
-    
 }
