49f122b20f5e70b0ab682416bd02837a848138b4
trishit <trishit.org@gmail.com>
2026-01-09 16:07:15 +0530

    Time: 10 ms (35.31%) | Memory: 49.6 MB (64.08%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,28 +1,25 @@
 import java.util.HashMap;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<Double,ArrayList<String>> check = new HashMap<>();
-        for(String str:strs)
-        {
-            double mapping = 0;
-            for(int j = 0;  j < str.length() ; j++)
-            {
-                mapping += Math.exp(str.charAt(j) - 96);
-            }
-            mapping = Math.round(mapping* 100)/100.0;
-            ArrayList<String> temp = check.getOrDefault(mapping,null);
-            if(temp == null)
-            {
-                temp = new ArrayList<String>();
-                temp.add(str);
-                check.put(mapping,temp);
-            }
-            else
-            temp.add(str);
- 
-        }
-        List<List<String>> result = new ArrayList<>(check.values());
-        return result;
+    HashMap<String,List<String>> check = new HashMap<>();
 
+    for(String str:strs)
+    {
+        char letters[] = str.toCharArray();
+        Arrays.sort(letters);
+        String key = Arrays.toString(letters);
+        if(check.containsKey(key))
+        check.get(key).add(str);
+        else
+        {
+            List<String> temp = new ArrayList<>();
+            temp.add(str);
+            check.put(key,temp);
+        }
+    }
+    List<List<String>> result = new ArrayList<>(check.values());
+        return result;
+    
     }
 }
