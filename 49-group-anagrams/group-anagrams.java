89425fd7148f210be0cd3410a918c409fd2ddb96
trishit <trishit.org@gmail.com>
2026-01-09 15:37:48 +0530

    Time: 19 ms (21.00%) | Memory: 50.2 MB (20.94%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<Double,ArrayList<String>> check = new HashMap<>();
+        for(String str:strs)
+        {
+            double mapping = 0;
+            for(int j = 0;  j < str.length() ; j++)
+            {
+                mapping += Math.exp(str.charAt(j) - 96);
+            }
+            mapping = Math.round(mapping*100)/100.0;
+            ArrayList<String> temp = check.getOrDefault(mapping,null);
+            if(temp == null)
+            {
+                temp = new ArrayList<String>();
+                temp.add(str);
+                check.put(mapping,temp);
+            }
+            else
+            temp.add(str);
+ 
+        }
+        System.out.println(check);
+        List<List<String>> result = new ArrayList<>(check.values());
+        return result;
+
+    }
+}
