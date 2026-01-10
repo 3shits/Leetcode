a705d4f8582c07f5026965ee680822c68f76627e
trishit <trishit.org@gmail.com>
2026-01-10 21:04:02 +0530

    Time: 14 ms (99.76%) | Memory: 54.8 MB (43.22%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -22,49 +22,49 @@ class MyHashMap {
     }
     
     public void put(int key, int value) {
-        System.out.println("Put:"+key+","+value);
+        // System.out.println("Put:"+key+","+value);
         int index = key % len;
         // System.out.println("put: "+index);
         if(map[index] == null)
         {
-        map[index] = new Bucket(key,value);
+            map[index] = new Bucket(key,value);
+            return;
+        }
+        if(map[index].key == key)
+        {
+            map[index].value = value;
+            return;
         }
         else
         {
-            if(map[index].key == key)
-            {
-                map[index].value = value;
-                return;
-            }
             Bucket temp = map[index];
             while(temp.next != null)
             {
                 if(temp.key == key)
                 {
-                    System.out.println("Updated");
+                    // System.out.println("Updated");
                 temp.value = value;
                 return;
                 }
                 temp = temp.next;
             }
             temp.next = new Bucket(key ,value);
-            
         }
     }
     
     public int get(int key) {
         int index = key % len;
-        System.out.println("get: "+key);
+        // System.out.println("get: "+key);
         if(map[index] == null)
         {
-        return -1;
+            return -1;
         }
         else
         {
-            System.out.println("Head: "+ map[index].key +","+map[index].value);
+            // System.out.println("Head: "+ map[index].key +","+map[index].value);
             if(map[index].key == key)
             {
-                System.out.println("Found: "+ map[index].key +","+map[index].value);
+                // System.out.println("Found: "+ map[index].key +","+map[index].value);
             return map[index].value;
             }
             else
@@ -72,10 +72,10 @@ class MyHashMap {
                 Bucket temp = map[index];
                 while(temp != null)
                 {
-                    System.out.println("Others: "+ temp.key +","+temp.value);
+                    // System.out.println("Others: "+ temp.key +","+temp.value);
                     if(temp.key == key)
                     {
-                    System.out.println("Found: "+ temp.key +","+temp.value);
+                    // System.out.println("Found: "+ temp.key +","+temp.value);
                     return temp.value;
                     }
                     temp = temp.next;
@@ -86,32 +86,26 @@ class MyHashMap {
     }
     
     public void remove(int key) {
-        System.out.println("Remove:"+key);
+        // System.out.println("Remove:"+key);
         int index = key % len;
         if(map[index] == null)
         return;
+            
+        if(map[index].key == key)
+        {
+            map[index] = map[index].next;   
+        }
         else
         {
-            
-            if(map[index].key == key)
+            Bucket temp = map[index];
+            while(temp.next != null)
             {
-                if(map[index].next != null)
-                map[index] = map[index].next;
-                else
-                map[index] = null;
-            }
-            else
-            {
-                Bucket temp = map[index];
-                while(temp.next != null)
+                if(temp.next.key == key)
                 {
-                    if(temp.next.key == key)
-                    {
-                        temp.next = temp.next.next;
-                        return;
-                    }
-                    temp = temp.next;
+                    temp.next = temp.next.next;
+                    return;
                 }
+                temp = temp.next;
             }
         }
     }
