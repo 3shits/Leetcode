9041e616a0b77d818984034043fe511aeaa1e112
trishit <trishit.org@gmail.com>
2026-01-10 21:09:56 +0530

    Time: 14 ms (99.76%) | Memory: 54.5 MB (45.75%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -22,9 +22,7 @@ class MyHashMap {
     }
     
     public void put(int key, int value) {
-        // System.out.println("Put:"+key+","+value);
         int index = key % len;
-        // System.out.println("put: "+index);
         if(map[index] == null)
         {
             map[index] = new Bucket(key,value);
@@ -42,7 +40,6 @@ class MyHashMap {
             {
                 if(temp.key == key)
                 {
-                    // System.out.println("Updated");
                 temp.value = value;
                 return;
                 }
@@ -54,30 +51,21 @@ class MyHashMap {
     
     public int get(int key) {
         int index = key % len;
-        // System.out.println("get: "+key);
         if(map[index] == null)
-        {
-            return -1;
-        }
+        return -1;
         else
         {
-            // System.out.println("Head: "+ map[index].key +","+map[index].value);
             if(map[index].key == key)
-            {
-                // System.out.println("Found: "+ map[index].key +","+map[index].value);
             return map[index].value;
-            }
+
             else
             {
                 Bucket temp = map[index];
                 while(temp != null)
                 {
-                    // System.out.println("Others: "+ temp.key +","+temp.value);
                     if(temp.key == key)
-                    {
-                    // System.out.println("Found: "+ temp.key +","+temp.value);
                     return temp.value;
-                    }
+
                     temp = temp.next;
                 }
                 return -1;
@@ -86,11 +74,9 @@ class MyHashMap {
     }
     
     public void remove(int key) {
-        // System.out.println("Remove:"+key);
         int index = key % len;
         if(map[index] == null)
         return;
-            
         if(map[index].key == key)
         {
             map[index] = map[index].next;   
