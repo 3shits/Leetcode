40a527064d32bc9c4430a1f7fc6f87bc04206592
trishit <trishit.org@gmail.com>
2026-01-12 13:16:40 +0530

    Time: 18 ms (13.75%) | Memory: 47.6 MB (53.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -32,11 +32,11 @@ class Solution {
             Map.Entry<Integer,Integer> temp = pqueue.get(i);
             pqueue.set(i,pqueue.get(smallest));
             pqueue.set(smallest,temp);
-
             heapifyDown(pqueue,pqueue.size(), smallest);
-        }
-            
+        }  
     }
+
+
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
         List<Map.Entry<Integer,Integer>> pqueue = new ArrayList<>();
@@ -53,13 +53,11 @@ class Solution {
             if(pqueue.size() > k)
             {
             Map.Entry<Integer,Integer> temp = pqueue.get(len - 1);
-            pqueue.set(len -1, pqueue.get(0));
             pqueue.set(0, temp);
             pqueue.remove(len-1);
             heapifyDown(pqueue,pqueue.size(),0);
             }
         }
-        System.out.println(pqueue);
         for(int i = 0 ; i<k ;i++)
         res[i] = pqueue.get(i).getKey();
         return res;
