8eb0f17c3d68fd9edc0659a40f0cc82924ddfe96
trishit <trishit.org@gmail.com>
2026-05-16 13:50:57 +0530

    Time: 1 ms (100.00%) | Memory: 71.1 MB (71.05%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -18,10 +18,7 @@ class Solution {
                 nums[ind] = nums[ind] != 0 ?-nums[ind] : -(nums.length + 1);
             }
         }
-    for(int i:nums)
-    {
-        System.out.print(i+" ");
-    }
+
         for(int i = 1 ; i <= nums.length ; i++)
         {
             if(nums[i-1] >= 0)
