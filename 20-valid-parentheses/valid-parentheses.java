6856efd3d15ebd9430d68aebab150258536ecdaf
trishit <trishit.org@gmail.com>
2026-08-09 16:23:20 +0530

    Time: 1 ms (99.92%) | Memory: 45 MB (5.70%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        char stack[] = new char[10000];
+        int tail = 0;
+        for(char c: s.toCharArray())
+        {
+            if(tail < -1)
+            break;
+
+            if(c == ')'&& stack[tail] == '(') 
+                --tail;
+
+            else if(c == '}'&& stack[tail] == '{')
+                --tail;
+            else if(c == ']'&& stack[tail] == '[')
+                --tail; 
+            else
+            stack[++tail] = c;
+        }
+
+        return tail==0;
+    }
+}
